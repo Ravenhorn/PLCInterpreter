4b8b0f8d291681f82862f635346098035cc4c417
Stuart Long <srl51@case.edu>
2013-02-22 18:45:16 -0500

    Added most of the stuff he gave us in class, as well as bind and lookup

--- interpreter.scm
+++ interpreter.scm
@@ -1,1 +1,50 @@
 (load "verySimpleParser.scm")
+
+(define interpret
+  (lambda (filename)
+    (lookup 'return (interpret_sl (parser filename) ('((return)))))))
+
+(define interpret_sl
+  (lambda (ptree env)
+    (cond
+      ((null? ptree) env)
+      (else (interpret_sl (cdr ptree) (interpret_stmnt (car ptree) env))))))
+
+(define interpret-stmnt
+  (lambda (stmnt env)
+    (cond
+      ((eq? '= (car statement)) (pret_assign stmnt env))
+      ((eq? 'var (car statement)) (pret_declare stmnt env))
+      ((eq? 'if (car statement)) (pret_if stmnt env))
+      ((eq? 'return (car statement)) (pret_return stmnt env))
+      ((operator? (car statement)) (pret_op stmnt env))
+      (else (error "invalid operator")))))
+      
+(define operator?
+  (lambda (op)
+    (cond
+      ((eq? '+ op) #t)
+      ((eq? '- op) #t)
+      ((eq? '* op) #t)
+      ((eq? '/ op) #t)
+      ((eq? '% op) #t)
+      ((eq? '|| op) #t)
+      ((eq? '&& op) #t)
+      ((eq? '! op) #t)
+      (else #f))))
+
+(define lookup
+  (lambda (var env)
+    (cond
+      ((null? env) (error "variable not declared"))
+      ((eq? var (caar env))
+       (cond
+         ((null? (cdar env)) (error "variable declared but not initialized"))
+         (else (cadar env))))
+      (else (lookup var (cdr env))))))
+
+(define bind
+  (lambda (var val env)
+    (cond
+      ((or (number? val) (boolean? val)) (cons (cons var (cons val '())) env))
+      (else (error "invalid type, variables must be an integer or boolean")))))
