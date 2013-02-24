e5139140a99bc9b45fc343f1cca0b9614b8ee443
Stuart Long <srl51@case.edu>
2013-02-24 01:17:56 -0500

    finished most o my stuff

--- interpreter.scm
+++ interpreter.scm
@@ -18,32 +18,55 @@
       ((eq? 'if (car statement)) (pret_if stmnt env))
       ((eq? 'return (car statement)) (pret_return stmnt env))
       (else (error "invalid operator")))))
+
+(define pret_return
+  (lambda (stmnt env)
+    (bind 'return (car (value (cadr stmnt) env)) (cadr (value (cadr stmnt) env)))))
      
 (define pret_if
   (lambda (stmnt env)
-    ()))
+    (cond
+      ((null? (cdddr stmnt))
+       (cond
+         ((car (eval_if (cadr stmnt) env)) (interpret_sl (caddr stmnt) (cadr (eval_if (cadr stmnt) env))))
+         (else env)))
+      (else
+       (cond
+         ((car (eval_if (cadr stmnt) env)) (interpret_sl (caddr stmnt) (cadr (eval_if? (cadr stmnt) env))))
+         (else (interpret_sl (caddr stmnt) (cadr (eval_if? (cadr stmnt) env)))))))))
 
+(define eval_if
+  (lambda (if env)
+    (cond
+      ((null? (cddr if)) (cons ((getBool (car if)) (car (value (cadr if) env))) (cons (cadr (value (cadr if) env)) '())))
+      (else (cons ((getBool (car if)) (car (value (cadr if) env)) (car (value (caddr if) (cadr (value (cadr if) env))))) 
+                  (cons (cadr (value (caddr if) (cadr (value (cadr if) env)))) '()))))))
+                                      
 (define getBool
   (lambda (op)
     (cond
-      ((eq? '>) >)
-      ((eq? '==) =)
-      ((eq? '<) <)
-      ((eq? '!=) (lambda (n1 n2) (not (= n1 n2))))
-      ((eq? '<=) <=)
-      ((eq? '>=) >=)
+      ((eq? '> op) >)
+      ((eq? '== op) =)
+      ((eq? '< op) <)
+      ((eq? '!= op) (lambda (n1 n2) (not (= n1 n2))))
+      ((eq? '<= op) <=)
+      ((eq? '>= op) >=)
+      ((eq? '|| op) (lambda (b1 b2) (or b1 b2)));for some reason just returning or gives a syntax error
+      ((eq? '&& op) (lambda (b1 b2) (and b1 b2)));for some reason just returning or gives a syntax error
+      ((eq? '! op) not)
       (else (error "invalid bool operator")))))
 
 ;TODO sideeffects
 (define value
   (lambda (expr env)
     (cond
-      ((number? expr) expr)
-      ((not (pair? expr)) (lookup expr env))
-      ((null? (cdr expr)) (value (car expr)))
+      ((or (number? expr) (boolean? expr)) (cons expr (cons env '())))
+      ((not (pair? expr)) (cons (lookup expr env) (cons env '())))
+      ((null? (cdr expr)) (value (car expr) env))
      ; ((eq? '= (car expr)) 
-      (else ((getOp (car expr)) (value (cadr expr)) (value (caddr expr)))))))
-  
+      (else (cons ((getOp (car expr)) (car (value (cadr expr) env)) (car (value (caddr expr) (cadr (value (cadr expr) env))))) 
+                  (cons (cadr (value (caddr expr) (cadr (value (cadr expr) env)))) '()))))))
+
 (define getOp
   (lambda (op)
     (cond
@@ -52,8 +75,8 @@
       ((eq? '* op) *)
       ((eq? '/ op) quotient)
       ((eq? '% op) remainder)
-      ((eq? '|| op) or)
-      ((eq? '&& op) and)
+      ((eq? '|| op) (lambda (b1 b2) (or b1 b2)));for some reason just returning or gives a syntax error
+      ((eq? '&& op) (lambda (b1 b2) (and b1 b2)));for some reason just returning or gives a syntax error
       ((eq? '! op) not)
       (else (error "error in getOp, operator not found")))))
 
@@ -83,7 +106,7 @@
 (define bind
   (lambda (var val env)
     (cond
-      ((null?
+      ((null? val) (cons (cons var '()) env))
       ((or (number? val) (boolean? val)) (cons (cons var (cons val '())) env))
       (else (error "invalid type, variables must be an integer or boolean")))))
 
