3d0e8d19c4229e3c48c36e352cabe66f4eae6a50
Jason Kuster <unexpectedapocalypse@gmail.com>
2013-04-30 00:56:05 -0400

    added more stuff
    Signed-off-by: Jason Kuster <unexpectedapocalypse@gmail.com>

--- interpreter_environment.scm
+++ interpreter_environment.scm
@@ -169,7 +169,7 @@
 
 (define make-def-const
   (lambda (env name)
-    (bind name '(() () (lambda (var) (error "trying to recurse on the constructor")) (lambda (var) (error "Trying to find the constructor's class"))) env)))
+    (bind name '(() () (lambda (var) (new-env)) (lambda (var) (new-env))) env)))
 
 (define add-exception-val
   (lambda (val env)
